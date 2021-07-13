bd5ae382aed617f64e65fe03c6dc2a38055f5625
chenxingyu0830 <chenxingyu0830@gmail.com>
2021-07-13 08:01:30 +0800

    Feat: name传参

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./importAllIcons";
-import './icon.scss';
+import "./icon.scss";
 import classes from "./helpers/classnames";
 
 interface IconProps extends React.SVGAttributes<SVGSVGElement> {
 	name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
-	const { className, ...restProps } = props;
+const Icon: React.FunctionComponent<IconProps> = props => {
+	const { className, name, ...restProps } = props;
 	return (
 		<svg className={classes("simple-icon", className)} {...restProps}>
-			<use xlinkHref={`#${props.name}`}></use>
+			<use xlinkHref={`#${name}`}></use>
 		</svg>
 	);
 };
