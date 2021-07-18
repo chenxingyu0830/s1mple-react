816818c79d8093a9a78b509a1e81706090bee50c
chenxingyu0830 <chenxingyu0830@gmail.com>
2021-07-19 01:06:18 +0800

    Feat: npm publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,17 +1,1 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import Icon from "./icon/icon"
-
-const fn: React.MouseEventHandler = (e) => {
-    console.log(e.target);
-}
-
-ReactDom.render(<div>
-    <Icon
-        name="weChat"
-        // className="weChat"
-        onClick={fn}
-        onMouseMove={_ => console.log("move")}
-        onMouseLeave={_ => console.log("leave")}
-    />
-</div>, document.querySelector('#root'));
+export {default as Icon} from "./icon/icon";
