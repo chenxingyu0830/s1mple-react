95c282470db0a950ae7ac8dfe107e36dbc1f3b67
chenxingyu0830 <chenxingyu0830@gmail.com>
2021-07-18 22:24:00 +0800

    Feat: 新增Coverage

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
     verbose: true,
     clearMocks: false,
     collectCoverage: true,
-    // collectCoverageFrom: ["{lib,include}/**/*.{js,jsx,ts,tsx}", "!**/node_modules/**"],
-    // coverageDirectory: 'coverage',
-    // coverageReporters: ['text', 'lcov'],
-    reporters: ["default", "jest-junit"],
+    collectCoverageFrom: ["{lib,include}/**/*.{js,jsx,ts,tsx}", "!**/node_modules/**"],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov'],
+    reporters: ["default"],
     // globals: {
     //   'ts-jest': {
     //     tsConfig: 'tsconfig.test.json',
