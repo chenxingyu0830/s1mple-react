e0b86f585c929bb6be31126bd6a403449f28ae2c
chenxingyu0830 <chenxingyu0830@gmail.com>
2021-07-27 23:58:27 +0800

    Feat: dev mode support debug

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,9 @@
 const base = require("./webpack.config");
+const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = Object.assign({}, base, {
 	mode: "development",
+	devtool: 'source-map',
 	entry: {
 		example: "./example.tsx",
 	},
