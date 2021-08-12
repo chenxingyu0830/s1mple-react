5822ba38bb1dbe7aa84b395c9fa5cc2c992762d2
chenxingyu0830 <chenxingyu0830@gmail.com>
2021-08-12 20:28:40 +0800

    Fix: 文字描述问题

--- lib/doc/install.tsx
+++ lib/doc/install.tsx
@@ -6,7 +6,7 @@ const InstallDoc: React.FunctionComponent = () => {
             <h1>安装</h1>
             <div>
                 <h2>提示</h2>
-                <p>注意，s1mple-react 采用 React Hooks,仅支持 React 16.8.0 后的版本。如果你在使用 React 16.8.0 之前的版本，可以去看看别的库。</p>
+                <p>注意，s1mple-react 采用 React Hooks，仅支持 React 16.8.0 后的版本。如果你在使用 React 16.8.0 之前的版本，可以去看看别的库。</p>
             </div>
             <div>
                 <h2>npm</h2>
